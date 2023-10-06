5455bd0daf6d99350d32c75cc952a29a66f6dc09
Dev_Pelz <pelzfx1@gmail.com>
2023-10-06 02:28:34 +0100

    modified:   test/Counter.t.sol

--- contracts/MerkleAirdrop.sol
+++ contracts/MerkleAirdrop.sol
@@ -2,37 +2,36 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Merkle {
-    bytes32 public merkleRoot =
-        0x953ccfc66eded938902be7f24ea1485af6541b376648c7794fc2c1a0268b1dd4;
-    address tokenAddress;
+contract Merkle is ERC20 {
+    bytes32 public merkleRoot;
 
     error AlreadyClaimed();
     error NotWhitelisted();
 
     mapping(address => bool) claimed;
 
-    constructor(address _tokenAddress) {
-        tokenAddress = _tokenAddress;
+    constructor(bytes32 _root) ERC20("Merkle", "MKT") {
+        merkleRoot = _root;
     }
 
     function claimToken(
         bytes32[] calldata _merkleProof,
+        address _account,
         uint256 _amount
     ) external returns (bool) {
-        if (claimed[msg.sender]) {
+        if (claimed[_account]) {
             revert AlreadyClaimed();
         }
-        bytes32 leaf = keccak256(abi.encodePacked(msg.sender, _amount));
+        bytes32 leaf = keccak256(abi.encodePacked(_account, _amount));
         if (!MerkleProof.verify(_merkleProof, merkleRoot, leaf)) {
             revert NotWhitelisted();
         }
 
         claimed[msg.sender] = true;
 
-        IERC20(tokenAddress).transfer(msg.sender, _amount);
+        _mint(_account, _amount);
         return true;
     }
 }
